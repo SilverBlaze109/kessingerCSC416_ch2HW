9e65c5699df01b3998a1db159f8a85e642549067
jacobKessinger <jacobkessinger@live.com>
2022-01-28 15:52:17 -0500

    project 2

--- HIITFit/Views/ContentView.swift
+++ HIITFit/Views/ContentView.swift
@@ -34,8 +34,16 @@ import SwiftUI
 
 struct ContentView: View {
   var body: some View {
-    Text("Hello, world!")
-      .padding()
+    TabView
+      {
+          WelcomeView()
+          ForEach(0 ..< 4)
+          {
+              x in
+              ExerciseView(index: x)
+          }
+      }
+      .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
   }
 }
 
